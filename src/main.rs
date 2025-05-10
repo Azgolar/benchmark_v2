b445bfa6dea1815c14f5b1d4fb5b88d47a1f9d34
Azgolar <azgolar2@googlemail.com>
2025-05-10 14:10:28 +0200

    10.05.25 mittag

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::{fs, env, path::Path};
+use std::{env, fs, path::Path, process::Command};
 use core_affinity;
 use getopts::Options;
 
 #[derive(Debug)]
-struct Einstellungen 
+struct Settings 
 {
     programm: String,   // Name des auszuführenden Programms
     kerne: Vec<i32>,    // Kerne für das Pinning
     n: Vec<i32>,        // Eingabegrößen für Benchmarking
+    t: i32,             // Anzahl der Threads für Benchmarking
     log: String,         // Name der Logdatei
     flagge: bool         // Ausgabe der Einstellungen
 }
@@ -21,14 +22,68 @@ struct ProzessorSpecs
    pub threads: i32       // Anzahl der Threads
 }
 
+fn starten(einstellungen: &Settings) 
+{
+    // Argumente formatieren
+    let formatiert = |v: &Vec<i32>| 
+    {
+        v.iter().map(|x| x.to_string()).collect::<Vec<_>>().join(",")
+    };
+
+    let kerne_formatiert = formatiert(&einstellungen.kerne);
+    let n_formatiert     = formatiert(&einstellungen.n);
+
+    // Programm starten und Ausgabe einlesen
+    let output = Command::new(format!("./{}", einstellungen.programm))
+        .arg(format!("[{}]", kerne_formatiert))
+        .arg(format!("[{}]", n_formatiert))
+        .arg(einstellungen.t.to_string())
+        .output();
+
+    match output 
+    {
+        Ok(out) => 
+        {
+            if out.status.success() 
+            {
+                println!("\nBenchmarking erfolgreich beendet\n");
+
+                // Benchmarking Programm gibt Laufzeit als String zurück
+                let rückgabe = String::from_utf8_lossy(&out.stdout).trim().to_string();
+
+                // umwandeln
+                let laufzeit: Vec<i32> = rückgabe.split(',').filter_map(|s| s.trim()
+                    .parse::<i32>().ok()).collect();
+                
+                // Debug
+                println!("Rückgabe-Vektor: {:?}", laufzeit);
+            } 
+            else 
+            {
+                println!("\nBenchmarking wurde mit Fehler beendet: {}\n", out.status);
+                std::process::exit(1);
+            }
+        }
+        Err(fehler) => 
+        {
+            println!("\nFehler beim Starten des Benchmarking-Programms: {}\n", fehler);
+            std::process::exit(1);
+        }
+    }
+}
+
+
 fn main() 
 {
     let prozessor: ProzessorSpecs = ProzessorSpecs::new();
-    let einstellungen: Einstellungen = Einstellungen::new();
+    let einstellungen: Settings = Settings::new();
 
+    // Debug
     println!("{:#?}", einstellungen);
-
     println!("{:#?}", prozessor);
+
+    // benchmarking starten
+    starten(&einstellungen);
 
 
 }
@@ -36,7 +91,7 @@ fn main()
 /*
     Parsen der übergebenen Paremeter 
 */
-impl Einstellungen 
+impl Settings 
 {
     pub fn new() -> Self 
     {
@@ -47,17 +102,19 @@ impl Einstellungen
         parameter.optopt("b", "", "", "");
         parameter.optopt("c", "", "", "");
         parameter.optopt("d", "", "", "");
+        parameter.optopt("e", "", "", "");
         // optinale Parameter
-        parameter.optflag("e", "", "");
+        parameter.optflag("f", "", "");
         parameter.optflag("h", "", "");
 
         // Test-Einstellungen
         let test_args: Vec<String> = vec![
             "-a".into(), "kette.txt".into(),        
-            "-b".into(), "15-19".into(),            
+            "-b".into(), "15-19".into(),         
             "-c".into(), "[1,2,3]".into(),
-            "-d".into(), "log".into(),
-            "-e".into(),
+            "-d".into(), "4".into(),   
+            "-e".into(), "log".into(),
+            "-f".into(),
             ];
         let gefunden = parameter.parse(&test_args).unwrap();
 
@@ -72,40 +129,47 @@ impl Einstellungen
             println!("-a <Name des auszuführenden Programms>");
             println!("-b <Kern ids für das Pinning: Format [1,7,3,5] oder 3-7>");
             println!("-c <Eingabegrößen n für das auszuführende Programm. Format: [10,80,30,100]>");
-            println!("-d <Name der Logdatei zum Speichern der Ergebnisse>");
+            println!("-d <Anzahl Threads für das Benchmarking>");
+            println!("-e <Name der Logdatei zum Speichern der Ergebnisse>");
             println!("\noptional:");
-            println!("-e <Ausgabe der CPU Spezifikationen");
+            println!("-f <Ausgabe der CPU Spezifikationen");
             std::process::exit(0);
         }
 
         // Parameter a parsen
         let programm: String = gefunden.opt_str("a").unwrap_or_else(|| 
-            { Einstellungen::fehlerausgabe("Parameter a nicht gefunden")});
+            Settings::fehlerausgabe("Parameter a nicht gefunden"));
         if !Path::new(&programm).is_file() 
         {
-             Einstellungen::fehlerausgabe("das auszuführende Programm existiert nicht");
+             Settings::fehlerausgabe("das auszuführende Programm existiert nicht");
         }
 
         // Parameter b parsen
         let b: String = gefunden.opt_str("b").unwrap_or_else(|| 
-            { Einstellungen::fehlerausgabe("Parameter b wurde nicht gefunden")});
-        let kerne: Vec<i32> = Einstellungen::kern_umwandeln(&b).unwrap_or_else(|_| 
-            { Einstellungen::fehlerausgabe("Parameter b hat falsches Format")});
+            Settings::fehlerausgabe("Parameter b wurde nicht gefunden"));
+        let kerne: Vec<i32> = Settings::kern_umwandeln(&b).unwrap_or_else(|_| 
+            Settings::fehlerausgabe("Parameter b hat falsches Format"));
 
         // Parameter c parsen
         let c: String = gefunden.opt_str("c").unwrap_or_else(|| 
-            { Einstellungen::fehlerausgabe("Parameter c wurde nicht gefunden")});
-        let n: Vec<i32> = Einstellungen::n_umwandeln(&c).unwrap_or_else(|_| 
-            { Einstellungen::fehlerausgabe("Parameter c hat falsches Format")});
+            Settings::fehlerausgabe("Parameter c wurde nicht gefunden"));
+        let n: Vec<i32> = Settings::n_umwandeln(&c).unwrap_or_else(|_| 
+            Settings::fehlerausgabe("Parameter c hat falsches Format"));
 
         // Parameter d parsen
-        let log: String = gefunden.opt_str("d").unwrap_or_else(|| 
-            { Einstellungen::fehlerausgabe("Parameter d nicht gefunden")});
+        let d: String = gefunden.opt_str("d").unwrap_or_else(|| 
+            Settings::fehlerausgabe("Parameter d nicht gefunden"));
+        let t: i32 = d.parse::<i32>().unwrap_or_else(|_| 
+            Settings::fehlerausgabe("Parameter d hat falsches Format"));
 
         // Parameter e parsen
-        let flagge: bool = gefunden.opt_present("e");
+        let log: String = gefunden.opt_str("e").unwrap_or_else(|| 
+            Settings::fehlerausgabe("Parameter e nicht gefunden"));
 
-        Einstellungen { programm, kerne, n, log, flagge}
+        // Parameter f parsen
+        let flagge: bool = gefunden.opt_present("f");
+
+        Settings { programm, kerne, n, t, log, flagge}
     }
 
     // Hilfsfunktion für Fehlerausgabe
